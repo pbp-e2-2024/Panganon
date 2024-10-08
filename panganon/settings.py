eb1fb2d0ec5628a29b8906441eb03c47bed2cf8d
Brian Altan <brianaltan28@gmail.com>
2024-10-08 21:14:52 +0700

    add allowed_hosts

--- panganon/settings.py
+++ panganon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v^=&9_(-sxik9!7ru39c!l*t5h6r*jwi8_our1v-n4nf+sc+&x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","brian-altan-panganon.pbp.cs.ui.ac.id"]
 
 # Application definition
 INSTALLED_APPS = [

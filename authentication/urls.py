1f7f2c2e6c64166333d621d50735fbc97d9f803b
Rosanne Valerie <valerierosanne@gmail.com>
2024-10-23 16:26:35 +0700

    buat authentication

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import login_user, register, show_main, logout_user
+
+urlpatterns = [
+    path('', login_user, name='login_user'),
+    path('login', login_user, name='login_user'),  # Optional: Jika Anda ingin memiliki path login yang eksplisit
+    path('register', register, name='register'),
+    path('main', show_main, name='show_main'),  # Tambahkan ini jika belum ada
+    path('logout', logout_user, name='logout_user'),
+]

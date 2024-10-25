09492cdb3475f0b5e9e9df5b68ae2cf1e64bb899
gselwas <gselwas@gmail.coml>
2024-10-25 23:55:52 +0700

    push

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    halal_ststaus = models.BooleanField(default=False)
+    operating_hours = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.name
+    
+class FavoriteRestaurant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+class RestaurantReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    review = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)

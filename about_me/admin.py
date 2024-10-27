9b6de69298f149d6b03af31553164840d547752e
Brian Altan <brianaltan28@gmail.com>
2024-10-27 18:39:41 +0700

    add admin power

--- about_me/admin.py
+++ about_me/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'food_preferences')
+    search_fields = ('user__username', 'bio', 'food_preferences')
+
+admin.site.register(UserProfile, UserProfileAdmin)

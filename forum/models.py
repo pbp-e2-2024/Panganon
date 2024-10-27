25ea0f954b0d006d859b2424823b0adf0d5432fb
Brian Altan <brianaltan28@gmail.com>
2024-10-27 22:20:51 +0700

    fix forum timezone formatting

--- forum/models.py
+++ forum/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from authentication.models import User
+from datetime import timedelta
 
 class Thread(models.Model):
     title = models.CharField(max_length=255)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def created_at_plus_7_hours(self):
+        return self.created_at + timedelta(hours=7)
+
 class Post(models.Model):
     thread = models.ForeignKey(Thread, related_name='posts', on_delete=models.CASCADE)
     content = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    def created_at_plus_7_hours(self):
+        return self.created_at + timedelta(hours=7)
+
     def get_top_level_comments(self):
         return self.comments.filter(parent__isnull=True)
 
@@ -21,3 +28,6 @@ class Comment(models.Model):
     content = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def created_at_plus_7_hours(self):
+        return self.created_at + timedelta(hours=7)

1c687707ce300589ee6fdc90fcaa8d66b5fef193
Brian Altan <brianaltan28@gmail.com>
2024-10-27 21:34:33 +0700

    load dataset through django admin dashboard

--- daftar_toko/admin.py
+++ daftar_toko/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.urls import path
+from django.core.management import call_command
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import Restaurant
 
-# Register your models here.
+class MyModelAdmin(admin.ModelAdmin):
+
+    change_list_template = "change_list.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('load-dataset/', self.admin_site.admin_view(self.load_dataset), name='load-dataset'),
+        ]
+        return custom_urls + urls
+
+    def load_dataset(self, request):
+        try:
+            call_command('load_makanan', 'dataset/Bika_Ambon_Medan.json', 'dataset/Durian_Medan.json', 
+                         'dataset/Kari_Medan.json', 'dataset/Lontong_Medan.json', 
+                         'dataset/Nasi_Lemak_Medan.json', 'dataset/Sate_Medan.json', 
+                         'dataset/Soto_Medan.json')
+            self.message_user(request, "Datasets loaded successfully", messages.SUCCESS)
+        except Exception as e:
+            self.message_user(request, f"Error loading datasets: {e}", messages.ERROR)
+        return HttpResponseRedirect("../")
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['custom_button'] = True
+        return super().changelist_view(request, extra_context=extra_context)
+
+admin.site.register(Restaurant, MyModelAdmin)

39758b491eb8ca3018fdb675e1137eda6749abcd
ruzbehan <baqliruzbehan@gmail.com>
2024-12-22 16:33:56 +0700

    Harusnya sudah finish

--- event/urls.py
+++ event/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('<int:pk>/', views.event_detail, name='event_detail'),
-    path('event/create/', views.event_create, name='event_create'),
+    path('create/', views.event_create, name='event_create'),
+    path('create-flutter/', views.event_create_flutter, name='event_create_flutter'),
     path('<int:pk>/edit/', views.event_edit, name='event_edit'),
+    path('<int:pk>/edit-flutter/', views.event_edit_flutter, name='event_edit_flutter'),
     path('<int:pk>/delete/', views.event_delete, name='event_delete'),
+    path('<int:pk>/delete-flutter/', views.event_delete_flutter, name='event_delete_flutter'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,21 +1,27 @@
 # event/views.py
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 from authentication.models import User  # Impor User dari modul authentication
 from .models import Event
 from .forms import EventForm
+from .serializers import EventSerializer
 
 def event_list(request):
-    event = Event.objects.all()
+    event = Event.objects.select_related('created_by').all()
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        event_data = list(event.values())
-        return JsonResponse(event_data, safe=False)
+        event_data = EventSerializer(event, many=True)
+        return JsonResponse(event_data.data, safe=False)
     return render(request, 'event/event_list.html', {'event': event})
 
 def event_detail(request, pk):
     # Mendapatkan detail event berdasarkan primary key (pk)
     event = get_object_or_404(Event, pk=pk)
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        event_data = EventSerializer(event)
+        return JsonResponse(event_data.data, safe=False)
     return render(request, 'event/event_detail.html', {'event': event})
 
 def event_create(request):
@@ -25,10 +31,10 @@ def event_create(request):
         return redirect('login_user')
 
     if request.method == 'POST':
-        form = EventForm(request.POST)  
+        form = EventForm(request.POST)
         if form.is_valid():
             event = form.save(commit=False)
-            
+
             try:
                 user_id = request.session.get('user_id')  # Ambil user ID dari session
                 user = User.objects.get(id=user_id)  # Ambil user dari model `authentication.models.User`
@@ -48,7 +54,7 @@ def event_edit(request, pk):
     if 'user_id' not in request.session:
         messages.error(request, "Anda harus login terlebih dahulu.")
         return redirect('login_user')
-    
+
     event = get_object_or_404(Event, pk=pk)
     if request.method == 'POST':
         form = EventForm(request.POST, instance=event)
@@ -72,3 +78,59 @@ def event_delete(request, pk):
         return redirect('event_list')
     
     return render(request, 'event/event_delete_confirmation.html', {'event': event})
+
+
+@csrf_exempt
+def event_create_flutter(request):
+    # Cek jika user sudah login
+    if 'user_id' not in request.session:
+        messages.error(request, "Anda harus login terlebih dahulu.")
+        return JsonResponse({'success': False, 'errors': 'Unauthenticated'})
+
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        form = EventForm(body)
+        if form.is_valid():
+            event = form.save(commit=False)
+            try:
+                user_id = request.session.get('user_id')  # Ambil user ID dari session
+                user = User.objects.get(id=user_id)  # Ambil user dari model `authentication.models.User`
+                event.created_by = user
+            except User.DoesNotExist:
+                return JsonResponse({'success': False, 'errors': 'User tidak ditemukan.'})
+
+            event.save()
+            return JsonResponse({'success': True, 'event_id': event.pk})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+    return JsonResponse({'success': False, 'errors': 'Invalid Method'})
+
+@csrf_exempt
+def event_edit_flutter(request, pk):
+    if 'user_id' not in request.session:
+        messages.error(request, "Anda harus login terlebih dahulu.")
+        return JsonResponse({'success': False, 'errors': 'Unauthenticated'})
+
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        form = EventForm(body, instance=event)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True, 'event_id': event.pk})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    return JsonResponse({'success': False, 'errors': 'Invalid Method'})
+
+@csrf_exempt
+def event_delete_flutter(request, pk):
+    if 'user_id' not in request.session:
+        messages.error(request, "Anda harus login terlebih dahulu.")
+        return JsonResponse({'success': False, 'errors': 'Unauthenticated'})
+
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        event.delete()
+        return JsonResponse({'success': True, 'event_id': event.pk})
+    return JsonResponse({'success': False, 'errors': 'Invalid Method'})
